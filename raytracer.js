2d9ba9925921406ec8e15b0b977fec2c505af251
Vincent Tran <vincenttran7411@gmail.com>
2019-04-26 04:46:52 -0400

    move around enableAO

--- raytracer.js
+++ raytracer.js
@@ -18,7 +18,7 @@ class Ray {
 	origin() { return this.o; }
 }
 
-function render() {
+function render(enableA0 = true) {
 	// create canvas of size imageWidth x imageHeight and add to DOM
 	let canvas = document.createElement('canvas');
 	canvas.width = imageWidth;
@@ -41,7 +41,7 @@ function render() {
 				let x = i/imageWidth;
 				let y = (imageHeight-1-j)/imageHeight;
 				let ray = camera.getCameraRay(x,y);
-				let color = raytracing(ray, 0);
+				let color = enableA0 ? raytracingAO(ray,0) : raytracing(ray, 0);
 				setPixelColor(pixels, idx, color);
 			}
 		}
@@ -94,28 +94,81 @@ function raytracing(ray, depth) {
 	return color;
 }
 
+function raytracingAO(ray, depth){
+	let color = new THREE.Color(0,0,0);
+// ===YOUR CODE STARTS HERE===
+	let isect = rayIntersectScene(ray) //returns the intersection structure
+	if (isect != null){ 
+		// if there's reflectance and not max depth, recurse; sum of its transparency and reflectance
+		if ( (isect.material.kr != null || isect.material.kt != null) && (depth < maxDepth)){
+			if(isect.material.kr != null){
+				let reflDir =  reflect(ray.direction().clone().negate(), isect.normal);
+				let reflect_ray = new Ray(isect.position, reflDir)
+				// reflect_ray = new Ray(ray.origin(), reflect(ray.direction(), isect.normal).negate())
+				color.add( raytracingAO(reflect_ray,depth+1 ).clone().multiply(isect.material.kr) )
+			}
+			if(isect.material.kt != null){
+				let refractDir = refract(ray.d.clone(), isect.normal, isect.material.ior )
+				let refract_ray = new Ray(isect.position, refractDir)
+				color.add(raytracingAO(refract_ray,depth+1 ).clone().multiply(isect.material.kt))
+			}
+			// color.setRGB(reflDir.x, reflDir.y, reflDir.z);
+
+		}
+		else{
+			// color = new THREE.Color(0,0,0);
+			let num_valid_points = 0
+			let num_no_intersecions = 0
+			while(num_valid_points != 100){
+				// for (let i = 0; i<100; i++){
+					let u = Math.random()
+					let v = Math.random()
+					let theta = 2 * Math.PI * u
+					let phi = Math.acos(2*v - 1)
+					let x = Math.cos(theta) * Math.sin(phi) // cos(theta) * sin (phi)
+					let y = Math.sin(theta) * Math.sin(phi) // sin(theta) * sin (phi)
+					let z = Math.cos(phi) // cos(phi)
+					let rand_vector_direction = new THREE.Vector3(x,y,z); // random direction of a potential ray
+					if (rand_vector_direction.clone().dot(isect.normal) > 0){
+						num_valid_points += 1
+						let rand_ray = new Ray(isect.position, rand_vector_direction)
+						if(rayIntersectScene(rand_ray) == null){ // if no intersections then add 1
+							num_no_intersecions += 1
+						}
+					}
+				}
+			let AO = num_no_intersecions/num_valid_points 
+
+			let gray = new THREE.Color(0.5,0.5,0.5)
+			color.add(gray.multiplyScalar(AO))
+		}
+	}
+	else{
+		color = backgroundColor.clone() // if no intersection
+	}
+// ---YOUR CODE ENDS HERE---
+	return color;
+}
+
 /* Compute and return shading color given a ray and the intersection point structure. */
 function shading(ray, isect) {
 	let color = new THREE.Color(0,0,0);
 // ===YOUR CODE STARTS HERE===
-	//ambient light should be intensity times reflectance; but since intensity so low its kinda equiv
+
+
 	a_clone = ambientLight.clone()
 	if(isect.material.ka != null){
 		color.add(a_clone.multiply(isect.material.ka))
 	}
-
 	for(let i=0; i<lights.length; i++){
 		let ls = lights[i].getLight(isect.position)
 		let shadowRay = new Ray(isect.position, ls.direction);
 		let distToLight = (ls.position.clone().sub(isect.position)).length()
 		let shadow_isect = rayIntersectScene(shadowRay)
-		if (shadow_isect && shadow_isect.t < distToLight){ // if there is a shadow intersection 
-		// if (shadow_isect){ // if there is a shadow intersection 
-			//and the intersection length is shorter that the distance to the light. If the interesection length
-			// were further than the distance to the light, that means intersected something behidn the light
-			// let shadowRay_distance =  shadowRay.pointAt(shadow_isect.t).sub(shadowRay.origin).length()
-			// if(shadow_isect.t <= distToLight){
-				continue; // skip this iteration; we're done this loop iteration no need to do shading since shadowed	
+		if (shadow_isect && shadow_isect.t < distToLight){ 
+			// if there is a shadow intersection 
+			//and the intersection length is shorter that the distance to the light. If
+			continue; // skip this iteration; we're done this loop iteration no need to do shading since shadowed	
 			// }
 		}
 		else{
@@ -129,6 +182,7 @@ function shading(ray, isect) {
 				let diffuse = intensity_clone.multiply(isect.material.kd)
 				diffuse.multiplyScalar(Math.max(n.clone().dot(l), 0))
 				color.add(diffuse)
+
 			}
 
 			// if(isect.material.ks != null && isect.material.p != null){
@@ -140,6 +194,7 @@ function shading(ray, isect) {
 			}
 		}
 	}	
+	
 // ---YOUR CODE ENDS HERE---
 	return color;
 	// return new THREE.Color(isect.normal.x,isect.normal.y,isect.normal.z) //return normals
